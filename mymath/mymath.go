99f7b3db4193c68e71c8cee516d2a4cb2d26b395
Denis Kanafeev <166117510+DenisKanafeev@users.noreply.github.com>
2024-05-15 15:45:45 +0300

    Add files via upload

---
+++ mymath/mymath.go
@@ -0,0 +1,31 @@
+package mymath
+
+import "math"
+
+func Sqrt(num float64) float64 {
+	return math.Sqrt(num)
+}
+
+func Ceil(num float64) float64 {
+	return math.Ceil(num)
+}
+
+func Floor(num float64) float64 {
+	return math.Floor(num)
+}
+
+func Pow(num1, num2 float64) float64 {
+	return math.Pow(num1, num2)
+}
+
+func Max(num1, num2 float64) float64 {
+	return math.Max(num1, num2)
+}
+
+func Min(num1, num2 float64) float64 {
+	return math.Min(num1, num2)
+}
+
+func main() {
+
+}
